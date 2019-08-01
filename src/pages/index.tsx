1cd68d74c0be84ae2bd2c4a5522ddea6758e44fd
simjes <simjes91@gmail.com>
2019-08-01 15:13:00 +0200

    Remove unused page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
-import { Link } from 'gatsby';
 import React from 'react';
-
 import Image from '../components/Image';
 import Layout from '../components/Layout';
 import SEO from '../components/Seo';
@@ -14,7 +12,6 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 );
 
